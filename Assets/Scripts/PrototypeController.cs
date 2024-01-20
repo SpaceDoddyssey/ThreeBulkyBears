a7b61d8bf21d8765e5eaf5d914d13733bebb9b70
SpaceDoddyssey <cameronradodd@gmail.com>
2024-01-20 00:40:00 -0800

    Cleaned up the jumping code a bit

--- Assets/Scripts/PrototypeController.cs
+++ Assets/Scripts/PrototypeController.cs
@@ -6,23 +6,25 @@ public class PlayerController2 : MonoBehaviour
 {
     private Rigidbody2D rb;
     private CircleCollider2D cc;
-    //[SerializeField] private 
     public LayerMask platforms;
     public float circleRadius;
     public float castDistance;
     //[Range(0, 10f)] [SerializeField] private float speed = 0f;
-    
+
     //float horizontal = 0f;
     //float lastJumpY = 0f;
     //private bool isFacingRight = true;
-    
+
     //bigger jump when holding jump button for longer
     bool jump = false, jumpHeld = false;
-    
-    [Range(0, 15f)] [SerializeField] private float fallLongMult = 1f;
-    [Range(0, 15f)] [SerializeField] private float fallShortMult = 2f;
+
+    [Range(0, 15f)][SerializeField] private float fallLongMult = 1f;
+    [Range(0, 15f)][SerializeField] private float fallShortMult = 2f;
+    [Range(0, 15f)][SerializeField] private float jumpvel = 7f;
     //edit above values based on size of the bear
-    
+
+    private bool visualizeCircleCast = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -33,17 +35,9 @@ public class PlayerController2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isOnGround() && Input.GetKeyDown(KeyCode.Space)) {
-            jump = true;
-        }
-        if (!isOnGround() && Input.GetKeyDown(KeyCode.Space)) {
-            jumpHeld = true;                        
-        }
-        else {
-            jumpHeld = false;
-        }
+        jump = isOnGround() && Input.GetKeyDown(KeyCode.Space);
 
-        //checking if jump is short or high
+        jumpHeld = !isOnGround() && Input.GetKey(KeyCode.Space);
     }
 
     void FixedUpdate()
@@ -51,32 +45,30 @@ public class PlayerController2 : MonoBehaviour
         //universal jump logic
         if (jump)
         {
-            float jumpvel = 7f;
             rb.velocity = Vector2.up * jumpvel;
             jump = false;
         }
 
-        //high jump
-        if (jumpHeld && rb.velocity.y > 0)
+        if (rb.velocity.y > 0)
         {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallLongMult - 1) * Time.fixedDeltaTime;
+            //Long jump or short jump
+            float multiplier = jumpHeld ? fallLongMult : fallShortMult;
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (multiplier - 1) * Time.fixedDeltaTime;
         }
-        else if(!jumpHeld && rb.velocity.y > 0)
-        {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallShortMult - 1) * Time.fixedDeltaTime;
-        }
-        //low jump
     }
 
     //check if the bear is touching the ground
-    private bool isOnGround() {
-        if (Physics2D.CircleCast(transform.position, circleRadius, -transform.up, castDistance, platforms))
+    private bool isOnGround()
+    {
+        return Physics2D.CircleCast(transform.position, circleRadius, -transform.up, castDistance, platforms);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (visualizeCircleCast)
         {
-            return true;
-        }
-        else 
-        {
-            return false;
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position - new Vector3(0, castDistance, 0), circleRadius);
         }
     }
 }
