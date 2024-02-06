52dac65519de5219f4596698d8b90ae2f3eb8ec0
SpaceDoddyssey <cameronradodd@gmail.com>
2024-02-05 16:09:40 -0800

    Flag now can be set per level

--- Assets/Scripts/Flag.cs
+++ Assets/Scripts/Flag.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Flag : MonoBehaviour
 {
+    void Start()
+    {
+        transform.position = GameObject.Find("FlagLoc").transform.position;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
