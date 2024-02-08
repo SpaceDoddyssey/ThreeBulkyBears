4cd5e8e98a935ce5b360850e5ee435fd910d9e0c
SpaceDoddyssey <cameronradodd@gmail.com>
2024-02-07 17:57:22 -0800

    Camera won't go below the world

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,21 +1,27 @@
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public Transform bear;
     public Vector3 offset;
     public Camera maincamera;
+    public float minYVal;
 
-    void Start() 
+    void Start()
     {
         maincamera = GameObject.Find("Main Camera").GetComponent<Camera>();
         bear = GameObject.Find("Bear").transform;
-        offset = new Vector3 (0f, 0f, -10f);
+        offset = new Vector3(0f, 0f, -10f);
     }
 
     void Update()
     {
-        transform.position = new Vector3 (bear.position.x + offset.x, bear.position.y + offset.y, offset.z);
+        transform.position = new Vector3(bear.position.x + offset.x, bear.position.y + offset.y, offset.z);
+        if (transform.position.y < minYVal)
+        {
+            transform.position = new Vector3(transform.position.x, minYVal, transform.position.z);
+        }
     }
 
 }
