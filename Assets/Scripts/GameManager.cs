a43fae4183a1da47d348a8ad614cb3018106f28a
SpaceDoddyssey <cameronradodd@gmail.com>
2024-02-25 17:18:22 -0800

    Added buttons to switch languages

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -15,6 +16,28 @@ public class GameManager : MonoBehaviour
         else
         {
             DontDestroyOnLoad(gameObject);
+        }
+        LocalizationSettings.InitializationOperation.WaitForCompletion();
+        Debug.Log(LocalizationSettings.AvailableLocales.Locales[0].LocaleName);
+        Debug.Log(LocalizationSettings.AvailableLocales.Locales[1].LocaleName);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (LocalizationSettings.SelectedLocale == LocalizationSettings.AvailableLocales.Locales[1])
+                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+            else
+                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (LocalizationSettings.SelectedLocale == LocalizationSettings.AvailableLocales.Locales[2])
+                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+            else
+                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[2];
         }
     }
 
